d0b4a4676c78af6dff93c5db48a43377f98dfb5c
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-17 18:16:14 -0600

    required morgan for loggin

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello Express!");
@@ -8,6 +10,14 @@ app.get("/", (req, res) => {
 
 app.get("/burgers", (req, res) => {
   res.send("We have juicy cheese burgers!");
+});
+
+app.get("/pizza/pepperoni", (req, res) => {
+  res.send("Your pizza is on the way!");
+});
+
+app.get("/pizza/pineapple", (req, res) => {
+  res.send("We don't serve that here. Never call again!");
 });
 
 app.listen(8000, () => {
